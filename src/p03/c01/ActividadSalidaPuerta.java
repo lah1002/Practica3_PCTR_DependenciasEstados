9e6bb205d263c5a6343dbb29d0d92168dc406453
lah1002 <74243503+lah1002@users.noreply.github.com>
2021-04-13 09:44:12 +0200

    ActividadSalidaPuerta finalizada

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,5 +1,10 @@
 package src.p03.c01;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class ActividadSalidaPuerta implements Runnable{
 	
 	private static final int NUMSALIDAS = 20;
@@ -13,7 +18,15 @@ public class ActividadSalidaPuerta implements Runnable{
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		for (int i = 0; i < NUMSALIDAS; i ++) {
+			try {
+				parque.salirDelParque(puerta);
+				TimeUnit.MILLISECONDS.sleep(new Random().nextInt(5)*1000);
+			} catch (InterruptedException e) {
+				Logger.getGlobal().log(Level.INFO, "Salida interrumpida");
+				Logger.getGlobal().log(Level.INFO, e.toString());
+				return;
+			}
+		}		
 	}
 }
